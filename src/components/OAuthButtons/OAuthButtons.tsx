615357fe7affa2a9d0599e2c747d32675d6cf450
Jfelix61 <jfelix61@outlook.com>
2019-08-12 18:42:55 -0400

    Removed facebook button

--- src/components/OAuthButtons/OAuthButtons.tsx
+++ src/components/OAuthButtons/OAuthButtons.tsx
@@ -26,30 +26,6 @@ const StyledGoogleLogo = styled(GoogleLogo)`
   margin-right: 1.2rem;
 `;
 
-const FacebookButton = styled.a`
-  && {
-    background-color: #3f51b5; /*Facebook trademark color*/
-    color: #fff;
-
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    &:hover {
-      background-color: #5e6ec7;
-      color: #fff;
-      border-color: initial;
-    }
-
-    &:focus {
-      background-color: #5e6ec7;
-      color: #fff;
-      border-color: initial;
-    }
-  }
-`;
-
 const StyledIcon = styled(Icon)`
   margin-right: 1.2rem;
   font-size: 24px;
@@ -65,11 +41,6 @@ const OAuthButtons = ({ type }: { type?: string }) => {
         <StyledGoogleLogo />
         {type === "signin" ? "Sign in with Google" : "Sign up with Google"}
       </GoogleButton>
-
-      <FacebookButton className="ant-btn ant-btn-default ant-btn-lg ant-btn-block">
-        <StyledIcon type="facebook" />
-        {type === "signin" ? "Sign in with Facebook" : "Sign up with Facebook"}
-      </FacebookButton>
     </Wrapper>
   );
 };
