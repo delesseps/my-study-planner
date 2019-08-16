251609b917975150f8f642c552531d8de3d709e5
Jfelix61 <jfelix61@outlook.com>
2019-08-15 22:18:48 -0400

    [Fix] server url

--- src/api/agent.ts
+++ src/api/agent.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3001/api"
-    : process.env.REACT_SERVER_URL + "/api";
+    : process.env.REACT_SERVER_URL + "api";
 
 const instance = axios.create({
   baseURL: BASE_URL,
