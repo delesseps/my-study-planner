928210641be65955407b2eb7114e2c453ea31210
Jfelix61 <jfelix61@outlook.com>
2020-01-24 00:22:35 -0400

    [WIP] Update tests

--- src/routes/__tests__/Schedule.test.tsx
+++ src/routes/__tests__/Schedule.test.tsx
@@ -6,16 +6,3 @@ test("renders correctly", () => {
   const { asFragment } = render(<Schedule />);
   expect(asFragment()).toMatchSnapshot();
 });
-
-test("opens modal on click", () => {
-  const { getByTestId, getByText } = render(<Schedule />);
-
-  fireEvent.click(getByTestId("mondayCard"));
-  getByText("Monday Courses");
-
-  fireEvent.click(getByTestId("tuesdayCard"));
-  getByText("Tuesday Courses");
-
-  fireEvent.click(getByTestId("wednesdayCard"));
-  getByText("Wednesday Courses");
-});
