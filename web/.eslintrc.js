5711cb50287dce3a81d12aca9d71121a71677c51
Jfelix61 <jfelix61@outlook.com>
2020-05-27 17:05:17 -0400

    remove hooks eslint plugin

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,7 +3,6 @@ const path = require('path')
 module.exports = {
   extends: [
     'react-app',
-    'plugin:react-hooks/recommended',
     'plugin:jest-dom/recommended',
     'plugin:testing-library/recommended',
     'plugin:testing-library/react',
