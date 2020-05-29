aca2646492d1b529452bf556a24bc57e0a98a391
Jfelix61 <jfelix61@outlook.com>
2020-05-29 01:21:25 -0400

    Fix stubs now being activated

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,2 +1,17 @@
 import '@testing-library/cypress/add-commands'
 import './commands'
+
+Cypress.on('window:load', window => {
+  // Get webApp iframe
+  const docIframe = window.parent.document.getElementById(
+    "Your App: 'my-study-planner'",
+  )
+  const webAppWindow = docIframe.contentWindow
+
+  // Get current cypress server thats started
+  const server = Cypress.state().server
+  if (server) {
+    // bind server to our webApp window
+    server.bindTo(webAppWindow)
+  }
+})
