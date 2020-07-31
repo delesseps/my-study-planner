39607b1834e7a9fb4795659065410521a3774003
jfelix61 <rtfire456@hotmail.com>
2020-07-31 17:43:56 -0400

    Added logs

--- api/src/loaders/firebase.ts
+++ api/src/loaders/firebase.ts
@@ -6,6 +6,9 @@ const serviceAccount = config.firebase
 console.log(config.firebase)
 
 if (process.env.NODE_ENV === 'production') {
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n')
+
+  console.log(serviceAccount)
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as any),
   })
